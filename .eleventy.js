7f3a9ab226902e8828e99739865beea31b139b3c
Ernest Wambua <ernestwambua3@gmail.com>
2025-01-11 19:35:52 +0300

    Major fix

--- .eleventy.js
+++ .eleventy.js
@@ -4,24 +4,25 @@ module.exports = function (eleventyConfig) {
   // Add syntax highlighting
   eleventyConfig.addPlugin(syntaxHighlight);
 
+  eleventyConfig.addGlobalData("layout", "base.njk");
+
   // Handle asset copying
-  eleventyConfig.addPassthroughCopy("articles/images");
+  eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("assets");
 
   // Process Markdown links and images
   eleventyConfig.addTransform("content-processing", function (content) {
+    // Fix image paths if needed
+    content = content.replace(/!\[\[(.*?)\]\]/g, function (match, p1) {
+      return `<img src='/${p1}' />`;
+    });
+
     // Convert wiki-style links to regular markdown links
     content = content.replace(/\[\[(.*?)\]\]/g, function (match, p1) {
       const [title, alias] = p1.split("|");
       const displayText = alias || title;
-      const baseUrl = "https://articles.ewambua.com";
       const url = title === "index" ? "" : title.toLowerCase().replace(/ /g, "-");
-      return `<a href='${baseUrl}/${url.toLowerCase().replace(/ /g, "-")}'>${displayText}</a>`;
-    });
-
-    // Fix image paths if needed
-    content = content.replace(/!\[\[(.*?)\]\]/g, function (match, p1) {
-      return `<img src='/images/${p1}' />`;
+      return `<a href='/${url.toLowerCase().replace(/ /g, "-")}'>${displayText}</a>`;
     });
 
     return content;
@@ -40,12 +41,11 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: {
-      input: "articles",    // Look for source files in articles directory
+      input: ".",    // Look for source files in articles directory
       output: "_site",      // Output to _site directory
-      includes: "../_includes",  // Look for includes in parent directory
-      layouts: "../_includes/layouts"
-    },
-    pathPrefix: "/articles/",         // Update this if site is not at root of domain
+      includes: "_includes",  // Look for includes in parent directory
+      layouts: "_includes/layouts"
+    },         // Update this if site is not at root of domain
     templateFormats: ["md", "njk"],
     markdownTemplateEngine: "njk"
   };
